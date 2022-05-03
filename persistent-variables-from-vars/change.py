c564a89d647213bac0e4ab237a6e26a337d1589a
Evan Floyd <evan.floyd@idevops.io>
2022-05-02 18:46:02 -0700

    corrected change.py vars file location input

--- persistent-variables-from-vars/change.py
+++ persistent-variables-from-vars/change.py
@@ -1,6 +1,6 @@
 import os
 import json
-vars_file=open(os.environ["vars_file_location"], "r").read()
+vars_file=open(os.environ["INPUT_VARS_FILE_LOCATION"], "r").read()
 json_var_object = json.loads(vars_file)
 append_github_file = open(os.environ["GITHUB_ENV"], "a")
 for key in json_var_object:
