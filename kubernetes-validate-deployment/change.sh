b815f0c714faae3c476435d17483781ac7e61a1e
Grant Zukel <grant@sifchain.finance>
2022-05-09 20:28:45 -0600

    updating the k8s steps to also take the aws creds as it needs it for aws-iam-auth to get the rbac permissions'

--- kubernetes-validate-deployment/change.sh
+++ kubernetes-validate-deployment/change.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-
 NAMESPACE=$1
 APP_NAME=$2
-
+export AWS_ACCESS_KEY_ID=$(echo $INPUT_AWS_ACCESS_KEY_ID)
+export AWS_SECRET_ACCESS_KEY=$(echo $INPUT_AWS_SECRET_ACCESS_KEY)
+export AWS_DEFAULT_REGION=$(echo $INPUT_AWS_DEFAULT_REGION)
 kubectl rollout status deployment/${INPUT_APP_NAME} --namespace ${INPUT_NAMESPACE}
