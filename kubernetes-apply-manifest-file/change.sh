3d5eb2114ce908dfddf1172aadbbed66851b4073
Grant Zukel <grant@sifchain.finance>
2022-05-09 20:45:48 -0600

    updating the k8s steps to also take the aws creds as it needs it for aws-iam-auth to get the rbac permissions'

--- kubernetes-apply-manifest-file/change.sh
+++ kubernetes-apply-manifest-file/change.sh
@@ -4,5 +4,6 @@ MANIFEST_FILELOCATION=$2
 export AWS_ACCESS_KEY_ID=$(echo $INPUT_AWS_ACCESS_KEY_ID)
 export AWS_SECRET_ACCESS_KEY=$(echo $INPUT_AWS_SECRET_ACCESS_KEY)
 export AWS_DEFAULT_REGION=$(echo $INPUT_AWS_DEFAULT_REGION)
+aws eks update-kubeconfig --name ${INPUT_K8S_CLUSTER_NAME} --region ${INPUT_AWS_DEFAULT_REGION}
 kubectl create namespace ${INPUT_NAMESPACE} || echo "Namespace Exists"
 kubectl apply -f ${INPUT_MANIFEST_FILELOCATION} -n ${INPUT_NAMESPACE}
--- kubernetes-validate-deployment/change.sh
+++ kubernetes-validate-deployment/change.sh
@@ -4,4 +4,5 @@ APP_NAME=$2
 export AWS_ACCESS_KEY_ID=$(echo $INPUT_AWS_ACCESS_KEY_ID)
 export AWS_SECRET_ACCESS_KEY=$(echo $INPUT_AWS_SECRET_ACCESS_KEY)
 export AWS_DEFAULT_REGION=$(echo $INPUT_AWS_DEFAULT_REGION)
+aws eks update-kubeconfig --name ${INPUT_K8S_CLUSTER_NAME} --region ${INPUT_AWS_DEFAULT_REGION}
 kubectl rollout status deployment/${INPUT_APP_NAME} --namespace ${INPUT_NAMESPACE}
