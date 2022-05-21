b7be0c2c5ef0b1f851b1659fb4d82d898d0a9238
Evan Floyd <evan.floyd@idevops.io>
2022-05-21 12:14:07 -0700

    removed dockerfile and change.sh and converted action.yml to composite action

--- kubernetes-apply-manifest-file/change.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-echo $INPUT_MANIFEST | base64 -d > deployment.yaml
-cat deployment.yaml
-export AWS_ACCESS_KEY_ID=$(echo $INPUT_AWS_ACCESS_KEY_ID)
-export AWS_SECRET_ACCESS_KEY=$(echo $INPUT_AWS_SECRET_ACCESS_KEY)
-export AWS_DEFAULT_REGION=$(echo $INPUT_AWS_DEFAULT_REGION)
-aws eks update-kubeconfig --name ${INPUT_K8S_CLUSTER_NAME} --region ${INPUT_AWS_DEFAULT_REGION}
-kubectl create namespace ${INPUT_NAMESPACE} || echo "Namespace Exists"
-kubectl apply -f deployment.yaml -n ${INPUT_NAMESPACE}
