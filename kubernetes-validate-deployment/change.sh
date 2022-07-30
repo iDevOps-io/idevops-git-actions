e4688060884fe237a888a936b575b8f5dc0df889
Stephen <steveokard@gmail.com>
2022-07-29 21:23:30 -0400

    convert to composite (#20)
    * convert to composite
    
    * removed old files
    
    Co-authored-by: Stephen Card <scard@idevops.io>

--- kubernetes-validate-deployment/change.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-NAMESPACE=$1
-APP_NAME=$2
-export AWS_ACCESS_KEY_ID=$(echo $INPUT_AWS_ACCESS_KEY_ID)
-export AWS_SECRET_ACCESS_KEY=$(echo $INPUT_AWS_SECRET_ACCESS_KEY)
-export AWS_DEFAULT_REGION=$(echo $INPUT_AWS_DEFAULT_REGION)
-aws eks update-kubeconfig --name ${INPUT_KUBERNETES_CLUSTER_NAME} --region ${INPUT_AWS_DEFAULT_REGION}
-kubectl rollout status deployment/${INPUT_APP_NAME} --namespace ${INPUT_NAMESPACE}
