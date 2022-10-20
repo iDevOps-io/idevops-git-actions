aa59a8dafcf2018456957f5568198a6073efe89b
Damon Cone <dcone@idevops.io>
2022-10-19 18:18:25 -0700

    changed prod.sh

--- kubernetes_deployment_couchdb/prod.sh
+++ kubernetes_deployment_couchdb/prod.sh
@@ -4,3 +4,4 @@ sleep 10
 curl -X PUT http://localhost:5984/_user
 curl -X PUT http://localhost:5984/_replicator
 curl -X PUT http://localhost:5984/_global_changes
+curl -X PUT $HOST/_node/couchdb@localhost/_config/admins/root -d '"super_secret"'
