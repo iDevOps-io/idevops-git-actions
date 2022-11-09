fdf3e5fe2067ff34aba7b376314ee0cee9055e68
Dave Rex <drex@idevops.io>
2022-11-09 14:21:38 -0700

    update mongo-replica-start.sh

--- kubernetes_deployment_mongo/mongo-replica-start.sh
+++ kubernetes_deployment_mongo/mongo-replica-start.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
 HOSTNAME="$(cat)/etc/hostname"
-RUN_MONGO="$(mongo) --eval"
-RUN_RS_MONGO="rs.initiate({ _id: "rs0", version: 1, members: [ { _id: 0, host: "mongo-0.mongo.mongo.svc.cluster.local:27017" }, { _id: 1, host: "mongo-1.mongo.mongo.svc.cluster.local:27017" }, { _id: 2, host: "mongo-2.mongo.mongo.svc.cluster.local:27017" } ]})"
+#SERVICE_PATH=$(cat /etc/hosts | grep -i $HOSTNAME | awk '{print $2}')
+RS_MONGO_INITIATE=$(mongo --eval "rs.initiate(
+   {
+      _id: \"rs0\",
+      version: 1,
+      members: [
+         { _id: 0, host : \"mongo-0.mongo.mongo.svc.cluster.local:27017\" },
+         { _id: 1, host : \"mongo-1.mongo.mongo.svc.cluster.local:27017\" },
+         { _id: 2, host : \"mongo-2.mongo.mongo.svc.cluster.local:27017\" }
+      ]
+   })")
 
-if [[ $HOSTNAME==mongo-0  ]]; then
-     echo 'mongo-0 is running'
- elif [[ $HOSTNAME==mongo-1 ]]; then
+if [[ $HOSTNAME==mongo-1  ]]; then
      echo 'mongo-1 is running'
+ elif [[ $HOSTNAME==mongo-2 ]]; then
+     echo 'mongo-2 is running'
  else
-     $RUN_MONGO $RUN_RS_MONGO 
+     sleep '5m' && $RS_MONGO_INITIATE
  fi
