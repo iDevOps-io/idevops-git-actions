6e7fca1bb62d1d248e4b64cf04519146b1f487b9
Dave Rex <drex@idevops.io>
2022-11-10 09:14:04 -0700

    cleaned formatting, changed script permissions.

--- kubernetes_deployment_mongo/mongo-replica-start.sh
+++ kubernetes_deployment_mongo/mongo-replica-start.sh
@@ -1,6 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
-#SERVICE_PATH=$(cat /etc/hosts | grep -i $HOSTNAME | awk '{print $2}')
-RS_MONGO_INITIATE=$(mongo --eval "rs.initiate({ _id: \"rs0\", version: 1, members: [{ _id: 0, host: \"mongo-0.mongo.mongo.svc.cluster.local:27017\" }, { _id: 1, host: \"mongo-1.mongo.mongo.svc.cluster.local:27017\" }, { _id: 2, host: \"mongo-2.mongo.mongo.svc.cluster.local:27017\" }]})")
+RS_MONGO_INITIATE=$(mongo --eval "rs.initiate(
+   {
+      _id: "rs0",
+      version: 1,
+      members: [
+         { _id: 0, host : "mongo-0.mongo.mongo.svc.cluster.local:27017" },
+         { _id: 1, host : "mongo-1.mongo.mongo.svc.cluster.local:27017" },
+         { _id: 2, host : "mongo-2.mongo.mongo.svc.cluster.local:27017" }
+      ]
+   }
+)"
+
 
 sleep '5m' && echo $RS_MONGO_INITIATE
