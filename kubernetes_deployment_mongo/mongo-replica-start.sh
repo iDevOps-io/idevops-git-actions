b947818d68320bfca2d79c70470288a430e984f5
Dave Rex <drex@idevops.io>
2022-11-11 12:21:44 -0700

    update script across all files

--- kubernetes_deployment_mongo/mongo-replica-start.sh
+++ kubernetes_deployment_mongo/mongo-replica-start.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-mongo --eval 'rs.initiate(
-   {
-      _id: "rs0",
-      version: 1,
-      members: [
-         { _id: 0, host : "mongo-0.mongo.mongo.svc.cluster.local:27017" },
-         { _id: 1, host : "mongo-1.mongo.mongo.svc.cluster.local:27017" },
-         { _id: 2, host : "mongo-2.mongo.mongo.svc.cluster.local:27017" }
-      ]
-   }
-)' 
+
+if [ $HOSTNAME == 'mongo-2' ]
+then
+  sleep '2m' && mongo --eval 'rs.initiate(
+     {
+        _id: "rs0",
+        version: 1,
+        members: [
+           { _id: 0, host : "mongo-0.mongo.mongo.svc.cluster.local:27017" },
+           { _id: 1, host : "mongo-1.mongo.mongo.svc.cluster.local:27017" },
+           { _id: 2, host : "mongo-2.mongo.mongo.svc.cluster.local:27017" }
+        ]
+     }
+  )'
+elif [ $HOSTNAME == 'mongo-0' ]
+then
+  touch "/etc/test.txt" && echo "pod one is running the script" >> "/etc/test.txt"
+else
+  echo "starting next pod"
+fi
