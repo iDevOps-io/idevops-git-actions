37a82afdc0c5b26a17c47a8873015b87810dc64b
Dave Rex <drex@idevops.io>
2022-11-13 14:22:09 -0700

    update script with variables

--- kubernetes_deployment_mongo/mongo-replica-start.sh
+++ kubernetes_deployment_mongo/mongo-replica-start.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 HOSTNAME=$(cat /etc/hostname)
+HOSTNAME_NO_NUMBER=$(cat /etc/hostname | awk -F"[-]+" '/-/{print $1}')
+CLUSTER_SERVICE_ADDRESS=$(cat /etc/hosts | grep $HOSTNAME | awk '{print $2}' | cut -f2- -d.)
 
-if [ $HOSTNAME == mongo-0 ]
+if [ $HOSTNAME == mongo-2 ]
 then
-  sleep '5m' && mongo --eval 'rs.initiate(
+  sleep '1m' && mongo --eval 'rs.initiate(
      {
         _id: "rs0",
         version: 1,
         members: [
-           { _id: 0, host : "mongo-0.mongo.mongo.svc.cluster.local:27017" },
-           { _id: 1, host : "mongo-1.mongo.mongo.svc.cluster.local:27017" },
-           { _id: 2, host : "mongo-2.mongo.mongo.svc.cluster.local:27017" }
+           { _id: 0, host : "'$HOSTNAME_NO_NUMBER'-0.'$CLUSTER_SERVICE_ADDRESS':27017" },
+           { _id: 1, host : "'$HOSTNAME_NO_NUMBER'-1.'$CLUSTER_SERVICE_ADDRESS':27017" },
+           { _id: 2, host : "'$HOSTNAME_NO_NUMBER'-2.'$CLUSTER_SERVICE_ADDRESS':27017" }
         ]
      }
   )'
-elif [ $HOSTNAME == mongo-1 ]
+elif [ $HOSTNAME == mongo-0 ]
 then
   echo "the script is running" >> /etc/test.txt
 else
